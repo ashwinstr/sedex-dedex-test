57caadd536de732c3945cc2d3f59268b29139709
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-15 00:39:23 +0530

    Update start.py

--- jutsu/plugins/start.py
+++ jutsu/plugins/start.py
@@ -8,6 +8,6 @@ async def start_(bot, message):
     reply_to = message.message_id
     await bot.send_message(
         message.chat.id,
-        f"Hello {message.from_user.first_name}, thank you for using this bot...",
+        f"Hello **{message.from_user.first_name}**, thank you for using this bot...",
         reply_to_message_id=reply_to
     )
