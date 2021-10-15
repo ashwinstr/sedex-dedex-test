0ed97e67adefd1a8ba54bfeee8b67406e08c8658
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-15 13:31:55 +0530

    Update start.py

--- jutsu/plugins/start.py
+++ jutsu/plugins/start.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 
 @Client.on_message(
-    filters.command(["start"], prefixes=";")
+    filters.command(["start"], prefixes=";"), group=1
 )
 async def start_(bot, message):
     reply_to = message.message_id
