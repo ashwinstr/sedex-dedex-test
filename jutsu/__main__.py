c366a7b0d830e4203e6904bc90bec272cd42c125
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-18 00:08:12 +0530

    Update __main__.py

--- jutsu/__main__.py
+++ jutsu/__main__.py
@@ -7,6 +7,14 @@ from decouple import config
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+HEROKU_API_KEY = config("HEROKU_API_KEY", default=None)
+HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+HEROKU_ENV = config(bool(int(("HEROKU_ENV", "0"))))
+HEROKU_APP = (
+        heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
+        if HEROKU_ENV and HEROKU_API_KEY and HEROKU_APP_NAME
+        else None
+    )
 
 
 if __name__ == "__main__" :
@@ -17,6 +25,7 @@ if __name__ == "__main__" :
         bot_token=BOT_TOKEN,
         api_id=APP_ID,
         api_hash=API_HASH,
+        
         plugins=plugins
     )
     app.run()
