73dd903ea17f76052a502977f9327bdc2e06c474
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-19 13:00:48 +0530

    Update main.py

--- jutsu/plugins/main.py
+++ jutsu/plugins/main.py
@@ -139,13 +139,13 @@ async def sed(bot, message):
             if check and check.group(0).lower() == to_fix.lower():
                  pass
             if "i" in flags and "g" in flags:
-                text = re.sub(fr"{repl}", f"{repl_with}", to_fix, flags=re.I).strip()
+                text = re.sub(fr"{repl}", fr"{repl_with}", to_fix, flags=re.I).strip()
             elif "i" in flags:
-                text = re.sub(fr"{repl}", f"{repl_with}", to_fix, count=1, flags=re.I).strip()
+                text = re.sub(fr"{repl}", fr"{repl_with}", to_fix, count=1, flags=re.I).strip()
             elif "g" in flags:
-                text = re.sub(fr"{repl}", f"{repl_with}", to_fix).strip()
+                text = re.sub(fr"{repl}", fr"{repl_with}", to_fix).strip()
             else:
-                text = re.sub(fr"{repl}", f"{repl_with}", to_fix, count=1).strip()
+                text = re.sub(fr"{repl}", fr"{repl_with}", to_fix, count=1).strip()
         except sre_err as e:
             return await bot.send_message(message.chat.id, f"**ERROR:** {e}")
 #            return await bot.send_message(message.chat.id, "**[Learn Regex](https://regexone.com)**")
