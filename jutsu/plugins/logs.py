51de1be9edb84d12204c48c6db73702c4f5ccc5c
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-19 15:58:55 +0530

    Update logs.py

--- jutsu/plugins/logs.py
+++ jutsu/plugins/logs.py
@@ -8,15 +8,14 @@ from .updater import HEROKU_APP
     group=3
 )
 async def logging_(bot, message):
-    await bot.send_message(message.chat.id, "`Checking logs...`")
+    msg_ = await bot.send_message(message.chat.id, "`Checking logs...`")
     try:
         limit = int((message.text).split()[1])
     except:
         limit = 100
     if HEROKU_APP:
         logs = (HEROKU_APP.get_log)(lines=limit)
-        await bot.send_document(
-            chat_id=message.chat.id,
+        await msg_.send_as_file(
             text=logs,
             filename="sedex-heroku.log",
             caption=f"sedex-heroku.log [ {limit} lines ]",
