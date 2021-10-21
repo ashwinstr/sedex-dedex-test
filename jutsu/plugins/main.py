b52e38fa4717f4ee0072a7267fecc940f4251f3b
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-21 13:29:19 +0530

    Update main.py

--- jutsu/plugins/main.py
+++ jutsu/plugins/main.py
@@ -86,8 +86,6 @@ async def sed(bot, message):
     sed_result = await separate_sed(og_text)
     if sed_result:
         repl, repl_with, flags = sed_result
-        repl = repl.html
-        repl_with = repl_with.html
 #        repl_with = unidecode(f"{repl_with}")
 #        await bot.send_message(message.chat.id, repl_with)
 #        repl_with = str(repl_with)
