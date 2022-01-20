02188072d43296c3be5ee510901a9cbff07dbf95
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-01-20 13:56:21 +0530

    Update main.py

--- jutsu/plugins/main.py
+++ jutsu/plugins/main.py
@@ -158,6 +158,10 @@ async def sed(bot, message):
     filters.regex(pattern="^[r]\/.*"), group=-2
 )
 async def no_reply_sed(bot, message):
+    try:
+        await bot.delete_messages(message.chat.id, message.message_id)
+    except:
+        pass
     text_ = message.text
     input_ = text_.split("/", 1)[1]
     await bot.send_message(message.chat.id, input_, disable_web_page_preview=True)
