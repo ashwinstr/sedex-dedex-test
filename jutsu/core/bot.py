64d2ff5e6aa2e7936a5d6a6d9aa33ea6ad8eb937
Kakashi <ashwinstr@gmail.com>
2022-01-27 18:14:43 +0530

    test

--- jutsu/core/bot.py
+++ jutsu/core/bot.py
@@ -3,16 +3,20 @@
 import asyncio
 import importlib
 import logging
+import signal
+import os
 
-from typing import List
+from typing import List, Optional, Awaitable, Any
 from types import ModuleType
 
-from pyrogram import Client
+from pyrogram import Client, idle
 
 from pyrogram.errors import MessageNotModified
 
 from jutsu import Config, logbot
 from jutsu.plugins import get_all_plugins
+
+from .ext import RawClient
 
 # logging.basicConfig(level=logging.INFO)
 
@@ -84,3 +88,52 @@ class Sedex(_AbstractUserge):
     async def sleep(self, msg):
         await msg.reply("`Sleeping for (10) Seconds.`")
         Config.HU_APP.restart()
+
+    def begin(self, coro: Optional[Awaitable[Any]] = None) -> None:
+        """ start userge """
+        lock = asyncio.Lock()
+        running_tasks: List[asyncio.Task] = []
+
+        async def _finalize() -> None:
+            async with lock:
+                for t in running_tasks:
+                    t.cancel()
+                if self.is_initialized:
+                    await self.stop()
+            # pylint: disable=expression-not-assigned
+            [t.cancel() for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+            await self.loop.shutdown_asyncgens()
+            self.loop.stop()
+            _LOG.info(_LOG_STR, "Loop Stopped !")
+
+        async def _shutdown(_sig: signal.Signals) -> None:
+            global _SEND_SIGNAL  # pylint: disable=global-statement
+            _LOG.info(_LOG_STR, f"Received Stop Signal [{_sig.name}], Exiting USERGE-X ...")
+            await _finalize()
+            if _sig == _sig.SIGUSR1:
+                _SEND_SIGNAL = True
+
+        for sig in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT, signal.SIGUSR1):
+            self.loop.add_signal_handler(
+                sig, lambda _sig=sig: self.loop.create_task(_shutdown(_sig)))
+        self.loop.run_until_complete(self.start())
+        for task in self._tasks:
+            running_tasks.append(self.loop.create_task(task()))
+        logbot.edit_last_msg("USERGE-X has Started Successfully !")
+        logbot.end()
+        mode = "[DUAL]" if RawClient.DUAL_MODE else "[BOT]" if Config.BOT_TOKEN else "[USER]"
+        try:
+            if coro:
+                _LOG.info(_LOG_STR, f"Running Coroutine - {mode}")
+                self.loop.run_until_complete(coro)
+            else:
+                _LOG.info(_LOG_STR, f"Idling USERGE-X - {mode}")
+                idle()
+            self.loop.run_until_complete(_finalize())
+        except (asyncio.exceptions.CancelledError, RuntimeError):
+            pass
+        finally:
+            self.loop.close()
+            _LOG.info(_LOG_STR, "Loop Closed !")
+            if _SEND_SIGNAL:
+                os.kill(os.getpid(), signal.SIGUSR1)
