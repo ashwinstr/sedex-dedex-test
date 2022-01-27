49605d57e246b5b439066a635171a92c84eb3e06
Kakashi <ashwinstr@gmail.com>
2022-01-27 15:19:05 +0530

    added error log

--- jutsu/plugins/started.py
+++ jutsu/plugins/started.py
@@ -1,5 +1,9 @@
 
+import logging
 from jutsu import Config, sedex
 
 async def _init() -> None:
-    await sedex.send_message(Config.LOG_CHANNEL_ID, "### **Sedex has started** ###")
+    try:
+        await sedex.send_message(Config.LOG_CHANNEL_ID, "### **Sedex has started** ###")
+    except Exception as e:
+        print(e)
