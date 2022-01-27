46fd9886d98501771b0b4b597f85a890996502be
Kakashi <ashwinstr@gmail.com>
2022-01-27 13:41:40 +0530

    added core for _init

--- jutsu/core/bot.py
+++ jutsu/core/bot.py
@@ -1,6 +1,9 @@
 
 
-import logging
+import asyncio
+import importlib
+from typing import List
+from types import ModuleType
 
 from pyrogram import Client
 
@@ -10,6 +13,35 @@ from jutsu import Config
 
 # logging.basicConfig(level=logging.INFO)
 
+
+_IMPORTED: List[ModuleType] = []
+_INIT_TASKS: List[asyncio.Task] = []
+
+
+async def _complete_init_tasks() -> None:
+    if not _INIT_TASKS:
+        return
+    await asyncio.gather(*_INIT_TASKS)
+    _INIT_TASKS.clear()
+
+
+class _AbstractUserge():
+    async def finalize_load(self) -> None:
+        """ finalize the plugins load """
+        await asyncio.gather(_complete_init_tasks(), self.manager.init())
+
+    async def load_plugin(self, name: str, reload_plugin: bool = False) -> None:
+        """ Load plugin to Userge """
+        _IMPORTED.append(
+            importlib.import_module(f"userge.plugins.{name}"))
+        if reload_plugin:
+            _IMPORTED[-1] = importlib.reload(_IMPORTED[-1])
+        plg = _IMPORTED[-1]
+        self.manager.update_plugin(plg.__name__, plg.__doc__)
+        if hasattr(plg, '_init'):
+            # pylint: disable=protected-access
+            if asyncio.iscoroutinefunction(plg._init):
+                _INIT_TASKS.append(self.loop.create_task(plg._init()))
 
 class Sedex(Client):
     def __init__(self):
