9758d684759e959a651b0651d96d272fde7dce73
Kakashi <ashwinstr@gmail.com>
2022-01-27 17:23:11 +0530

    term test

--- jutsu/config.py
+++ jutsu/config.py
@@ -5,10 +5,12 @@ from heroku3 import from_key
 
 class Config:
     API_ID = int(os.environ.get("APP_ID", 0))
-    API_HASH = os.environ.get("API_HASH", None)
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HU_APP = from_key(API_KEY).apps()[APP_NAME]
+    API_HASH = os.environ.get("API_HASH", None)
+    APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DB_URI = os.environ.get("DB_URL", None)
+    HU_APP = from_key(API_KEY).apps()[APP_NAME]
     LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    
--- jutsu/plugins/executor.py
+++ jutsu/plugins/executor.py
@@ -9,7 +9,7 @@ from os import geteuid
 from pyrogram import filters
 from pyrogram.types import Message
 from jutsu.helpers import telegrapher
-from jutsu import sedex, Sedex
+from jutsu import sedex, Sedex, Config
 
 
 @sedex.on_message(
@@ -66,3 +66,113 @@ async def eval_(sedex: Sedex, message: Message):
             await msg.edit(
                 text=output, parse_mode="md"
             )
+
+
+@sedex.on_message(
+    filters.command("term", prefixes=";")
+    & filters.user([Config.OWNER_ID]),
+    group=-2
+)
+async def term_(message: Message):
+    """run commands in shell (terminal with live update)"""
+    cmd = await init_func(message)
+    if cmd is None:
+        return
+    await message.edit("`Executing terminal ...`")
+    try:
+        t_obj = await Term.execute(cmd)  # type: Term
+    except Exception as t_e:  # pylint: disable=broad-except
+        await message.err(str(t_e))
+        return
+    curruser = getuser()
+    try:
+        uid = geteuid()
+    except ImportError:
+        uid = 1
+    output = f"{curruser}:~# {cmd}\n" if uid == 0 else f"{curruser}:~$ {cmd}\n"
+    count = 0
+    while not t_obj.finished:
+        count += 1
+        if message.process_is_canceled:
+            t_obj.cancel()
+            await message.reply("`process canceled!`")
+            return
+        await asyncio.sleep(0.5)
+        if count >= Config.EDIT_SLEEP_TIMEOUT * 2:
+            count = 0
+            out_data = f"<pre>{output}{t_obj.read_line}</pre>"
+            await message.try_to_edit(out_data, parse_mode="html")
+    out_data = f"<pre>{output}{t_obj.get_output}</pre>"
+    await message.edit_or_send_as_file(
+        out_data, parse_mode="html", filename="term.txt", caption=cmd
+    )
+
+
+async def init_func(message: Message):
+    cmd = message.input_str
+    if not cmd:
+        await message.err("No Command Found!")
+        return None
+    if "config.env" in cmd:
+        await message.err("That's a dangerous operation! Not Permitted!")
+        return None
+    return cmd
+
+
+class Term:
+    """live update term class"""
+
+    def __init__(self, process: asyncio.subprocess.Process) -> None:
+        self._process = process
+        self._stdout = b""
+        self._stderr = b""
+        self._stdout_line = b""
+        self._stderr_line = b""
+        self._finished = False
+
+    def cancel(self) -> None:
+        self._process.kill()
+
+    @property
+    def finished(self) -> bool:
+        return self._finished
+
+    @property
+    def read_line(self) -> str:
+        return (self._stdout_line + self._stderr_line).decode("utf-8").strip()
+
+    @property
+    def get_output(self) -> str:
+        return (self._stdout + self._stderr).decode("utf-8").strip()
+
+    async def _read_stdout(self) -> None:
+        while True:
+            line = await self._process.stdout.readline()
+            if line:
+                self._stdout_line = line
+                self._stdout += line
+            else:
+                break
+
+    async def _read_stderr(self) -> None:
+        while True:
+            line = await self._process.stderr.readline()
+            if line:
+                self._stderr_line = line
+                self._stderr += line
+            else:
+                break
+
+    async def worker(self) -> None:
+        await asyncio.wait([self._read_stdout(), self._read_stderr()])
+        await self._process.wait()
+        self._finished = True
+
+    @classmethod
+    async def execute(cls, cmd: str) -> "Term":
+        process = await asyncio.create_subprocess_shell(
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        t_obj = cls(process)
+        asyncio.get_event_loop().create_task(t_obj.worker())
+        return t_obj
